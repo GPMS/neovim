acc6492e4e9552b3e8fdd646a72fc5bbcbb37d51
GPMS <gab.portela@hotmail.com>
2021-12-21 15:38:45 -0300

    Don't show virtual text on LSP

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,7 +1,7 @@
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         underline = true,
-        virtual_text = true,
+        virtual_text = false,
         signs = true,
         update_in_insert = true,
     }
