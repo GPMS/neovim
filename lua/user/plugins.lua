f209967e1eefdcc9a5a020fe7a023f9d44a5856f
Gabriel Portela <gportelams@gmail.com>
2022-08-17 10:18:49 -0300

    plugins: Show message when installing packer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,8 +1,9 @@
 -- Auto install
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+if fn.empty(fn.glob(install_path)) > 0 then
+    print('Installing Packer...')
+    fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
     vim.cmd 'packadd packer.nvim'
 end
 
